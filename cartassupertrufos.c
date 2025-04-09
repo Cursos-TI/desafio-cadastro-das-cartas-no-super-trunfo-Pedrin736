a3064e96be18a1b21bf533cadfba1a1ac9244b91
Pedrin736 <pl145251@gmail.com>
2025-04-09 18:34:49 +0000

    novo projeto

--- cartassupertrufos.c
+++ cartassupertrufos.c
@@ -1,9 +1,36 @@
 #include <stdio.h>
 
 int main(){
-    printf("Cartas super trunfos!\n");
+char estado[50];
+char código da carta[50];
+char nome da cidade[50];
+int população;
+float àrea;
+float PIb;
+int Números de pontos turísticos;
 
+printf("Digite o nome do estado: \n");
+scanf("%s", &estado)
 
+printf("Digite o código da carta: \n");
+scanf("%s", &código da carta);
+
+printf("Digite o nome da cidade: \n");
+scanf("%s", &nome da cidade);
+
+printf("Digite o tamanho da população: \n");
+scanf("%f", &tamanho da população);
+
+printf("Digite o tamanho da àrea: \n");
+scanf("f", &àrea);
+
+printf("Digite o PIB: \n");
+scanf("%f", &PIB);
+
+printf("Digite os números de pontos turísticos");
+scanf("%d", &Números de pontos turísticos);
+
+printf("Nome do estado: %s - ")
 
 
 }
