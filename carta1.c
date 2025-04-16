4aad246cb074350c3a4e5f2c1ab131cc9da0287d
Pedrin736 <pl145251@gmail.com>
2025-04-16 13:03:11 +0000

    carta1

--- carta1.c
+++ carta1.c
@@ -5,7 +5,7 @@ int main(){
 char estado[10]; //A
 char codigo[20]; //A01
 char cidade[20]; //Riodejaneiro
-int populaçao;   // 200.000 
+int populacao;   // 200.000 
 float area;      // 2.000.000 km²
 float PIB;       // 50.000.000 bilhões
 int pontos_turisticos; // 50
@@ -20,7 +20,7 @@ printf("Digite a cidade: \n");
 scanf("%s", &cidade);
 
 printf("Digite a populaçao: \n");
-scanf("%d", &populaçao);
+scanf("%d", &populacao);
 
 printf("Digite a area: \n");
 scanf("%f", &area);
@@ -34,7 +34,7 @@ scanf("%d", &pontos_turisticos);
 print("estado: %s\n", estado);
 printf("codigo: %s\n", codigo);
 printf("cidade: %s\n", cidade);
-print("populaçao: %d\n", populaçao);
+print("populaçao: %d\n", populacao);
 print("area: %f\n", area);
 print("PIB: %f", PIB);
 print("pontos_turisticos: %d\n", pontos_turisticos);
