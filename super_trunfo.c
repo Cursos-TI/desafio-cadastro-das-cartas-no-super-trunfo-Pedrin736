e2e5710f18dd859c04034737d6f69e61f0a503fc
Pedrin736 <pl145251@gmail.com>
2025-04-23 16:03:03 +0000

    carta1

--- super_trunfo.c
+++ super_trunfo.c
@@ -3,7 +3,7 @@
 int main(){
     //carta1
 char estado; //A
-char codigo[20]; //A01
+char codigo[4]; //A01
 char nomeCidade[50]; //Riodejaneiro
 int numero_habitantes; // 200.000 
 float areaTotal; // 2.000.000 km²
@@ -29,7 +29,7 @@ printf("Digite o PIB1: \n");
 scanf("%lf", &PIB);
 
 printf("Digite os pontos_turisticos1: \n");
-scanf("%d", &pontos_turisticos1);
+scanf("%d", &pontos_turisticos);
 
 printf("estado: %s\n", estado);
 printf("codigo: %s\n", codigo);
