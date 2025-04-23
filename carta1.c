64683bacf24cf7d5b2d120d09059f7afaf966ae4
Pedrin736 <pl145251@gmail.com>
2025-04-23 04:16:01 +0000

    carta1

--- carta1.c
+++ carta1.c
@@ -4,11 +4,11 @@ int main(){
     //carta1
 char estado;//A
 char codigo[20]; //A01
-char cidade[20]; //Riodejaneiro
-int populacao;   // 200.000 
-float area;      // 2.000.000 km²
+char nomeCidade[500]; //Riodejaneiro
+int numero_habitantes;   // 200.000 
+float areaTotal;      // 2.000.000 km²
 float PIB;       // 50.000.000 bilhões
-int pontos_turisticos; // 50
+int pontos_turisticos1; // 50
 
 printf("Digite o estado: \n");
 scanf("%s", &estado);
