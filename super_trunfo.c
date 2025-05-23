d2c3e397d74914accb960ff082a6645e9554eb09
Pedrin736 <pl145251@gmail.com>
2025-05-23 15:47:50 +0000

    carta1

--- super_trunfo.c
+++ super_trunfo.c
@@ -6,8 +6,8 @@ char estado [10]; //A
 char codigo [7]; //A01
 char nomeCidade[50]; //Riodejaneiro
 int numero_habitantes; // 200.000 
-float pib; // 50.000.000 
 float areaTotal; // 2.000.000 km²
+float pib; // 50.000.000
 int pontos_turisticos1; // 50
 
 printf("Digite o estado: \n");
