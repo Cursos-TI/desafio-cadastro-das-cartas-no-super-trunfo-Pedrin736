c1a4b05fab69a6b5b0e8fdb1102ec5b3e7d06df0
Pedrin736 <pl145251@gmail.com>
2025-05-26 15:48:41 +0000

    carta1

--- super_trunfo.c
+++ super_trunfo.c
@@ -55,7 +55,7 @@ printf("pontos_turisticos1: %d\n", pontos_turisticos1);
  int numeros_habitantes2; // 40000
  float areaTotal2, pib2;
  int  pontos_turisticos2; // 23
- int c; // variavel para limpar o buffer
+ int c2; // variavel para limpar o buffer
 
 printf("Digite o estado2: \n");
  scanf("%s", &estado2);
