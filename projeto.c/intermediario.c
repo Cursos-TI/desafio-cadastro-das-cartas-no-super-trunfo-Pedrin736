3e9ef4becae82fec185fd54ae3bc5f2bad51637c
Pedrin736 <pl145251@gmail.com>
2025-05-29 15:01:31 +0000

    intermediaria

--- projeto.c/intermediario.c
+++ projeto.c/intermediario.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
  int main(){
-
+    /*
+     Soma (+)
+     Subtração (-)
+     Multiplicação (*)
+     Divisão (/)
+    */
+   
     int = numero1, numero2;
     int = soma, subtracao, multiplicacao, divisao;
 
