1a0dc22737f8b380d7ac53ecc19f1764a019a6e2
Pedrin736 <pl145251@gmail.com>
2025-05-29 14:52:40 +0000

    intermediario.c

---
+++ projeto.c/intermediario.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+ int main(){
+
+    int= numero1, numero2;
+    int = soma, subtracao, multiplicacao, divisao;
+
+    printf("Entre com o numero 1: \n");
+    scanf("%d", &numero1);
+    printf("Entre com o numero 2: \n");
+    scanf("%d", &numero2);
+    //operaçao soma
+    soma = numero1 + numero2;
+    
+    //operaçao subtracao
+    subtracao = numero1 - numero2;
+
+    //operaçao multiplicao
+    multiplicacao = numero1 * numero2;
+
+    //operaçao divisao
+    divisao = numero1 / numero2;
+
+
+    printf("A soma é: %d\n", soma);
+    printf("A subtracao é: %d\n", subtracao);
+    printf("A multiplicacao é: %d\n", multiplicacao);
+ }  printf("A divisao é: %d\n", divisao);
