80098e59f67c901057850f4ece3f67ebd1c0df7d
Pedrin736 <pl145251@gmail.com>
2025-05-29 17:30:51 +0000

    ccccccc.c

--- projeto.c/cccccc.c
+++ projeto.c/cccccc.c
@@ -16,14 +16,30 @@
   printf("Antes incremento: %d\n", numero1);
   //numero1 = numero1 +1; 
   //numero1 += 1;
+  //Pos incremento:
+  //resultado = numero1;
+  //numero1++;
  resultado = numero1++;
  //printf("Apos incremento: %d\n", numero1);
   printf("Apos pos-incremento - numero1: %d - resultado: %d\n", numero1, resultado);
 
+  resultado = ++numero1;
+  printf("Apos Pre-incremento - numero1: %d - resultado: %d\n", numero1, resultado);
+ 
   //numero1 = numero1 -1;
   //numero1 -= 1;
-  numero1 --;
-  printf("Apos decremento: %d\n", numero1);
+  //numero1 --;
+  //printf("Apos decremento: %d\n", numero1);
+ resultado = numero1--;
+  printf("Apos pos-decremento - numero1: %d, resultado: %d\n", numero1, resultado),
+
+ resultado = --numero1;
+  printf("Apos pre-decremento - numero1: %d - resultado: %d\n", numero1, resultado);
+
+
+
+
+ 
 
 
 
